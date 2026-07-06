add996368650ab07247acd8bbc986960990d4483
andrew <abdumalikovfourth@gmail.com>
2026-07-06 11:49:40 +0900

    feat: integration of fresh menu, events and complete publishing

--- src/index.tsx
+++ src/index.tsx
@@ -4,10 +4,10 @@ import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./app/App";
 import reportWebVitals from "./reportWebVitals";
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "./app/MaterialTheme";
 import { BrowserRouter as Router } from "react-router-dom";
-import theme from './app/MaterialTheme';
 import "./css/index.css";
 
 const container = document.getElementById("root")!;
