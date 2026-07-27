17f2a47b850943be9b1265fe231909d0b48cd613
andrew <abdumalikovfourth@gmail.com>
2026-07-27 17:38:20 +0900

    fix: retrieve top users and modify ActiveUsers component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -9,43 +9,49 @@ import "../../../css/home.css";
 
 import { useDispatch } from "react-redux";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setNewDishes, setPopularDishes } from "./slice";
+import { setNewDishes, setPopularDishes, setTopUsers } from "./slice";
 import { Product } from "../../../lib/types/product";
 import ProductService from "../../services/ProductService";
 import { ProductCollection } from "../../../lib/enums/product.enum";
+import MemberService from "../../services/MemberService";
+import { Member } from "../../../lib/types/member";
 
 /** REDUX: SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
   setPopularDishes: (data: Product[]) => dispatch(setPopularDishes(data)),
   setNewDishes: (data: Product[]) => dispatch(setNewDishes(data)),
+  setTopUsers: (data: Member[]) => dispatch(setTopUsers(data)),
 });
 
 export default function HomePage() {
-  const { setPopularDishes, setNewDishes } = actionDispatch(useDispatch());
+  const { setPopularDishes, setNewDishes, setTopUsers } = actionDispatch(useDispatch());
 
   useEffect(() => {
     const product = new ProductService();
+    const member = new MemberService();
+
     product
       .getProducts({
-        page: 1, 
+        page: 1,
         limit: 4,
-        order: "productViews",
+        order: "productView",
         productCollection: ProductCollection.DISH,
       })
-      .then((data) => {
-        setPopularDishes(data);
-      })
+      .then((data) => setPopularDishes(data))
       .catch((err) => console.log(err));
 
     product
       .getProducts({
-        page: 1, 
+        page: 1,
         limit: 4,
         order: "createdAt",
       })
-      .then((data) => {
-        setNewDishes(data);
-      })
+      .then((data) => setNewDishes(data))
+      .catch((err) => console.log(err));
+
+    member
+      .getTopUsers()
+      .then((data) => setTopUsers(data))
       .catch((err) => console.log(err));
 
   }, []);
